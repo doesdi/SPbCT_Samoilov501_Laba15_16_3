f5967390b67f964d1b391d77a2d538ab2f8bee7b
Husokka <samoilovzeka31@mail.ru>
2021-11-02 15:27:52 +0300

    Добавьте файлы проекта.

---
+++ SPbCT_Samoilov501_Laba15_16_3/MyForm.h
@@ -0,0 +1,195 @@
+﻿#pragma once
+#include <math.h>
+using namespace std;
+double a, b, S;
+namespace SPbCTSamoilov501Laba15163 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+/// Òðåáóåìûé ìåòîä äëÿ ïîääåðæêè êîíñòðóêòîðà — íå èçìåíÿéòå 
+/// ñîäåðæèìîå ýòîãî ìåòîäà ñ ïîìîùüþ ðåäàêòîðà êîäà.
+/// </summary>
+		void InitializeComponent(void)
+		{
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Controls->Add(this->textBox3);
+			this->groupBox1->Controls->Add(this->label5);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->groupBox1->Location = System::Drawing::Point(13, 13);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(352, 229);
+			this->groupBox1->TabIndex = 0;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Решение задачи: ";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(7, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(200, 23);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"S=1/(a+b) + 1/(a+b)^2";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(7, 67);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(173, 23);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Введите значения:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(186, 67);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(31, 23);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"a=";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(185, 105);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(32, 23);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"b=";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(223, 64);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(85, 32);
+			this->textBox1->TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(223, 102);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(85, 32);
+			this->textBox2->TabIndex = 5;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(7, 146);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(33, 23);
+			this->label5->TabIndex = 6;
+			this->label5->Text = L"S=";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(46, 143);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->Size = System::Drawing::Size(100, 32);
+			this->textBox3->TabIndex = 7;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(105, 190);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(134, 33);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"Рассчитать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(377, 248);
+			this->Controls->Add(this->groupBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->textBox3->Text = Convert::ToString("");
+		a = Convert::ToDouble(textBox1->Text);
+		b = Convert::ToDouble(textBox2->Text);
+		S = 1 / (a + b) + 1 / pow((a + b), 2);
+		this->textBox3->Text = Convert::ToString(S);
+	}
+	};
+}
